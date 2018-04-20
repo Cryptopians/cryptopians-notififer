064ee3d97700b558bbc60a4678f02821595db7eb
Rob Moorman <rob@moori.nl>
2018-04-20 16:55:34 +0200

    Fix send all new markets for new exchange after failing to fetch api

--- src/cn/app.py
+++ src/cn/app.py
@@ -22,12 +22,11 @@ def run():
             if client.has.get('publicAPI'):
                 try:
                     markets = client.load_markets()
+                    store.add_markets(client, markets)
                 except:
                     err = sys.exc_info()[0]
                     msg = "Couldn't fetch data from '%s': %s" % (client.name, err)
                     logger.warning(msg)
-                if markets:
-                    store.add_markets(client, markets)
         logger.info("Successfully verified data from %d exchanges" % len(ccxt.exchanges))  # noqa
         store.update_store()        
         time.sleep(int(settings.CN_INTERVAL))
